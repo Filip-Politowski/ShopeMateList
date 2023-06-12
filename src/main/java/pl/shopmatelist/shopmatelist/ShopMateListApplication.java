6e3ef0388c32fedcfd6aae7f2b40ab545688421c
Filip-Politowski <fil.politowski@gmail.com>
2023-06-12 20:00:19 +0200

    Initial commit

---
+++ src/main/java/pl/shopmatelist/shopmatelist/ShopMateListApplication.java
@@ -0,0 +1,13 @@
+package pl.shopmatelist.shopmatelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMateListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMateListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/shopmatelist/shopmatelist/ShopMateListApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.shopmatelist.shopmatelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMateListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
