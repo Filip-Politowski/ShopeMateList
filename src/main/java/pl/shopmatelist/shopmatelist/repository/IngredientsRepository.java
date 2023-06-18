f5dff8a72ce44463ed577ee11ca0586139ed017b
Filip-Politowski <fil.politowski@gmail.com>
2023-06-18 19:42:11 +0200

    added listing all products in Ingredients by recipe Id

--- src/main/java/pl/shopmatelist/shopmatelist/repository/IngredientsRepository.java
+++ src/main/java/pl/shopmatelist/shopmatelist/repository/IngredientsRepository.java
@@ -1,7 +1,16 @@
 package pl.shopmatelist.shopmatelist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.shopmatelist.shopmatelist.entity.Ingredients;
+import pl.shopmatelist.shopmatelist.entity.Products;
+
+import java.util.List;
 
 public interface IngredientsRepository extends JpaRepository<Ingredients, Long> {
+
+    @Query("SELECT i.product FROM Ingredients i WHERE i.recipe.recipeId = :recipeId")
+    List<Products> findProductsByRecipeId(@Param("recipeId") Long recipeId);
+
 }
