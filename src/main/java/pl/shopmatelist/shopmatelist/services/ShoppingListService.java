69e865727f4fd3d8a02b0037b66bab0df6e51f0f
Filip-Politowski <fil.politowski@gmail.com>
2023-06-20 20:09:04 +0200

    Added ShoppingListService

---
+++ src/main/java/pl/shopmatelist/shopmatelist/services/ShoppingListService.java
@@ -0,0 +1,54 @@
+package pl.shopmatelist.shopmatelist.services;
+
+import org.springframework.stereotype.Service;
+import pl.shopmatelist.shopmatelist.dto.ShoppingListDTO;
+import pl.shopmatelist.shopmatelist.entity.ShoppingList;
+import pl.shopmatelist.shopmatelist.mapper.ShoppingListMapper;
+import pl.shopmatelist.shopmatelist.repository.ShoppingListRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class ShoppingListService {
+
+    private final ShoppingListRepository shoppingListRepository;
+    private final ShoppingListMapper shoppingListMapper;
+
+    public ShoppingListService(ShoppingListRepository shoppingListRepository, ShoppingListMapper shoppingListMapper) {
+        this.shoppingListRepository = shoppingListRepository;
+        this.shoppingListMapper = shoppingListMapper;
+    }
+
+    public ShoppingListDTO findById(Long id) {
+        Optional<ShoppingList> optionalShoppingList = shoppingListRepository.findById(id);
+        if (optionalShoppingList.isPresent()) {
+            ShoppingList shoppingList = optionalShoppingList.get();
+            return shoppingListMapper.toDTO(shoppingList);
+        }
+        throw new NoSuchElementException();
+    }
+
+    public List<ShoppingListDTO> findAll() {
+        List<ShoppingList> shoppingLists = shoppingListRepository.findAll();
+        return shoppingListMapper.toDtoList(shoppingLists);
+    }
+
+    public ShoppingListDTO save(ShoppingListDTO shoppingListDTO) {
+        ShoppingList shoppingList = shoppingListMapper.toEntity(shoppingListDTO);
+        ShoppingList savedShoppingList = shoppingListRepository.save(shoppingList);
+        return shoppingListMapper.toDTO(savedShoppingList);
+    }
+
+    public void deleteById(Long id) {
+        shoppingListRepository.deleteById(id);
+    }
+
+    public ShoppingListDTO update(ShoppingListDTO shoppingListDTO) {
+        ShoppingList shoppingList = shoppingListMapper.toEntity(shoppingListDTO);
+        ShoppingList updatedShoppingList = shoppingListRepository.save(shoppingList);
+        return shoppingListMapper.toDTO(updatedShoppingList);
+    }
+
+}
