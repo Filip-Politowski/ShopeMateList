a5561417cfcff26049322f370a2e936bf782fce5
Filip-Politowski <fil.politowski@gmail.com>
2023-06-20 21:10:37 +0200

    Added ProductsOnListController

---
+++ src/main/java/pl/shopmatelist/shopmatelist/rest/ProductsOnListController.java
@@ -0,0 +1,43 @@
+package pl.shopmatelist.shopmatelist.rest;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pl.shopmatelist.shopmatelist.dto.ProductsOnListDTO;
+import pl.shopmatelist.shopmatelist.services.ProductsOnListService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class ProductsOnListController {
+
+    private final ProductsOnListService productsOnListService;
+
+    @GetMapping("/productsonlist/{id}")
+    public ProductsOnListDTO findProductsOnListById(@PathVariable Long id){
+        return productsOnListService.findById(id);
+    }
+
+    @GetMapping("/productsonlist")
+    public List<ProductsOnListDTO> findAllProductsOnList(){
+        return productsOnListService.findAll();
+    }
+
+    @PostMapping("/productsonlist")
+    public ProductsOnListDTO createProductOnList(@RequestBody ProductsOnListDTO productsOnListDTO){
+        return productsOnListService.save(productsOnListDTO);
+    }
+
+    @DeleteMapping("productsonlist/{id}")
+    public void deleteProductsOnList(@PathVariable Long id){
+        productsOnListService.deleteById(id);
+    }
+
+    @PutMapping("/productsonlist")
+    public ProductsOnListDTO updateProductOnList(@RequestBody ProductsOnListDTO productsOnListDTO){
+        return productsOnListService.update(productsOnListDTO);
+    }
+
+
+}
