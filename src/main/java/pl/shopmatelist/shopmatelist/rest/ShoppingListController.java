b8d07e31354663b2304570b242239e860b606a3d
Filip-Politowski <fil.politowski@gmail.com>
2023-06-20 21:18:27 +0200

    Added ShoppingListController

---
+++ src/main/java/pl/shopmatelist/shopmatelist/rest/ShoppingListController.java
@@ -0,0 +1,41 @@
+package pl.shopmatelist.shopmatelist.rest;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pl.shopmatelist.shopmatelist.dto.ShoppingListDTO;
+import pl.shopmatelist.shopmatelist.services.ShoppingListService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class ShoppingListController {
+
+    private final ShoppingListService shoppingListService;
+
+    @GetMapping("/shoppinglist/{id}")
+    public ShoppingListDTO findShoppingListById(@PathVariable Long id){
+        return shoppingListService.findById(id);
+    }
+    @GetMapping("/shoppinglist")
+    public List<ShoppingListDTO> findAllShoppingLists(){
+        return shoppingListService.findAll();
+    }
+
+    @PostMapping("/shoppinglist")
+    public ShoppingListDTO createShoppingList(@RequestBody ShoppingListDTO shoppingListDTO){
+        return shoppingListService.save(shoppingListDTO);
+    }
+
+    @DeleteMapping("/shoppinglist/{id}")
+    public void deleteShoppingList(@PathVariable Long id){
+        shoppingListService.deleteById(id);
+    }
+
+    @PutMapping("/shoppinglist")
+    public ShoppingListDTO updateShoppingList(@RequestBody ShoppingListDTO shoppingListDTO){
+        return shoppingListService.update(shoppingListDTO);
+    }
+
+}
