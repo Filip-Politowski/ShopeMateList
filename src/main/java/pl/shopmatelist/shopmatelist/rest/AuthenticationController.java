cbcd63b621e4f258322e74004fc34ac9609d5733
Filip-Politowski <fil.politowski@gmail.com>
2023-06-29 20:54:04 +0200

    Added AuthenticationController

---
+++ src/main/java/pl/shopmatelist/shopmatelist/rest/AuthenticationController.java
@@ -0,0 +1,31 @@
+package pl.shopmatelist.shopmatelist.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.shopmatelist.shopmatelist.dto.request.SignInRequest;
+import pl.shopmatelist.shopmatelist.dto.request.SignUpRequest;
+import pl.shopmatelist.shopmatelist.dto.response.JwtAuthenticationResponse;
+import pl.shopmatelist.shopmatelist.services.AuthenticationService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request){
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request){
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+}
