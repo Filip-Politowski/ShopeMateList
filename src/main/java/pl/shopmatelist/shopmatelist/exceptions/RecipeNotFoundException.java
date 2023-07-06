bba053de27bb2a3e2e4004eb8bc321fdfefa98d6
Filip-Politowski <fil.politowski@gmail.com>
2023-07-06 23:58:05 +0200

    Added exception handling in Recipes

--- src/main/java/pl/shopmatelist/shopmatelist/exceptions/RecipeNotFoundException.java
+++ src/main/java/pl/shopmatelist/shopmatelist/exceptions/RecipeNotFoundException.java
@@ -1,4 +1,7 @@
 package pl.shopmatelist.shopmatelist.exceptions;
 
-public class RecipeNotFoundException {
+public class RecipeNotFoundException extends RuntimeException{
+    public RecipeNotFoundException(String message) {
+        super(message);
+    }
 }
